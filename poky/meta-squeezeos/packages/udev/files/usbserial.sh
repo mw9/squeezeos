22c553280b578254ffd71d8e4af49945d90d36f2
Ralph Irving <ralph.irving@gmail.com>
2026-01-21 09:46:53 -0500

    Support removal of usb-serial-dongle via udev for touch only. Update usbserial.sh script to handle udev removal action.

--- poky/meta-squeezeos/packages/udev/files/usbserial.sh
+++ poky/meta-squeezeos/packages/udev/files/usbserial.sh
@@ -2,6 +2,27 @@
 #
 # Called from udev
 #
-# Start getty for usb-serial-dongles
+# Start/stop getty for usb-serial-dongles logins
+#
+port="`basename "$DEVNAME"`"
 
-$(while true; do /sbin/getty ttyUSB0 115200 VC vt100; done) &
+if [ -z "$port" ]; then
+	port=ttyUSB0
+fi
+
+if [ -z "$ACTION"]; then
+	ACTION=add
+fi
+
+case $ACTION in
+	add)
+		$(while true; do /sbin/getty $port 115200 VC vt100; done) &
+		;;
+	remove)
+		if [ -x /usr/bin/killall ]; then
+			killall "`basename "$0"`"
+		fi
+		;;
+esac
+
+exit 0
